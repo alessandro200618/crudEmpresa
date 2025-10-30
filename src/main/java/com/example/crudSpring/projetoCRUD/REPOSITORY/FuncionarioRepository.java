69a6dbc6a7e1d6b32606af1986b140106ed48cd0
Alessandro <alessandrolemespaula@gmail.com>
2025-10-30 20:36:17 -0300

    (30/10/25)

--- src/main/java/com/example/crudSpring/projetoCRUD/REPOSITORY/FuncionarioRepository.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/REPOSITORY/FuncionarioRepository.java
@@ -1,5 +1,16 @@
 package com.example.crudSpring.projetoCRUD.REPOSITORY;
 
-public interface FuncionarioRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.example.crudSpring.projetoCRUD.ENTITY.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario,Long> {
+
+    List<Funcionario> findByNomeContainingIgnoreCase(String nome_funcionario);
     
 }
